6e5f7ff936a550ee940c53834e30c807173e0307
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-03 11:42:50 +0100

    Add OpenCV drawing helpers for boxes, areas, and alerts

--- utils/draw_utils.py
+++ utils/draw_utils.py
@@ -3,6 +3,7 @@ import cv2
 def draw_bounding_box(frame, box, color):
     x1, y1, x2, y2 = box
     cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
+    
 def draw_crib_area(frame, crib_area, color):
     cv2.rectangle(frame, (crib_area[0], crib_area[1]),
                   (crib_area[2], crib_area[3]), color, 2)
